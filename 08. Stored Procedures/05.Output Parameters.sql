e2e98c5d2c7d7a5f3c340e957eef7ea0cdc1ea2a
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-01-17 15:35:13 +0300

    08. Stored Procedures

---
+++ 08. Stored Procedures/05.Output Parameters.sql
@@ -0,0 +1,20 @@
+USE sql_invoices;
+
+DELIMITER $$
+
+CREATE PROCEDURE get_unpaid_invoices_for_client
+(
+	client_id INT,
+    OUT invoices_count INT,
+    OUT invoices_total DECIMAL(9, 2)
+)
+
+BEGIN
+	SELECT COUNT(*), SUM(invoice_total)
+    INTO invoices_count, invoices_total
+    FROM invoices i
+    WHERE i.client_id = client_id
+		AND payment_total = 0;
+END$$
+
+DELIMITER ;
